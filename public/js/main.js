bcf41b5f08dda78b2d9a45daefcb66d9f0687b6a
joaoUA <joaop.oliveira@ua.pt>
2023-05-10 21:37:51 +0100

    adiciona redirecao para pagina leilao ao clicar em licitar na pagina inicial

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 const loginBtn = document.getElementById("login-btn")
 
+const licitarBtns = Array.from(document.getElementsByClassName("btn btn-dark bkg-terciary cl-primary fnt-m"))
+
 loginBtn.addEventListener("click", () => {
     window.location.href = "./perfilDados.html";
 })
+
+licitarBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        window.location.href = "./leilao.html"
+    })
+});
