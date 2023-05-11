a1f70f74b7a0d46b88d88860df4a60d3dc0575b2
joaoUA <joaop.oliveira@ua.pt>
2023-05-11 17:24:29 +0100

    modifica header para melhor responsividade
    reestruturar header
    remove links desnecessários da navbar
    acrescenta popup ao clicar no icon de pesquisa
    acrescenta popup ao clicar no icon de perfil
    no popup de perfil, menu com links para dados de perfil, criar leilao, e
    pagina de verificações

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const loginBtn = document.getElementById("login-btn")
-
 const licitarBtns = Array.from(document.querySelectorAll(".my-card-sub > a"))
+const searchIcon = document.getElementById("search-icon");
+const profileIcon = document.getElementById("profile-icon");
+
+const searchBar = document.getElementById("search-bar")
+const profileMenu = document.getElementById("profile-menu");
 
 loginBtn?.addEventListener("click", () => {
     window.location.href = "./perfilDados.html";
@@ -10,4 +14,12 @@ licitarBtns?.forEach(btn => {
     btn.addEventListener("click", () => {
         window.location.href = "./leilao.html"
     })
+});
+
+searchIcon?.addEventListener('click', () => {
+    searchBar?.classList.toggle('hidden');
+});
+
+profileIcon?.addEventListener('click', () => {
+    profileMenu?.classList.toggle('hidden')
 });
